9050242bb3a25c49062ed4b3908a07ad331c8d83
vicky lu <vicky.lu@IC-c02fq6m5md6r.attlocal.net>
2022-03-12 16:34:06 -0500

    adding the basic files

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+    var animation = lottie.loadAnimation({
+        container: document.getElementById('lottie'), // Required
+        path: './images/Comp1.json', // Required
+        renderer: 'svg/canvas/html', // Required
+        loop: true, // Optional
+        autoplay: true, // Optional
+        name: "Hello World", // Name for future reference. Optional.
+      })
+    
+}
